c1669eab5b45a47980da7b0cdde3a79cb8c858b0
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-01-10 21:35:35 -0200

    Create video_list and add it in index.js/Refactor code

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyCe7DUSCBFIblEmX0nSMS-xsWYQKDry3SQ';
 
@@ -11,8 +12,8 @@ class App extends Component {
 
     this.state = {videos: []};
   
-    YTSearch({key: API_KEY, term: 'surfboards'}, function(videos) {
-      this.setState({videos: videos});
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({videos}); // this.setState({videos: videos})
     });
 
   }
@@ -21,6 +22,7 @@ class App extends Component {
     return (
        <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
