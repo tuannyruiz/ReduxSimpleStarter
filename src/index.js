2942ba91ab363e1daa31d8d8d3c2a9923ca2afdc
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-01-10 21:08:34 -0200

    Replace functional component with a class based component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCe7DUSCBFIblEmX0nSMS-xsWYQKDry3SQ';
 
-// Create a new component. This component should produce some HTML.
-const App = () => {
-  return (
- 		<div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {videos: []};
+  
+    YTSearch({key: API_KEY, term: 'surfboards'}, function(videos) {
+      this.setState({videos: videos});
+    });
+
+  }
+
+  render() {
+    return (
+       <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
-// Take this component's generated HTML and put it on the page (in the DOM).
 ReactDOM.render(<App/>, document.querySelector('.container'));
