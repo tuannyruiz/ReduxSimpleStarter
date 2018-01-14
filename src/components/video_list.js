1d0ffaf9acb438a6ec6537926b33efa351a0bda9
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-01-14 17:56:37 -0200

    Add key in VideoListItem

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,11 +3,11 @@ import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
   const videoItems = props.videos.map((video) => {
-    return <VideoListItem video={video} />;
+    return <VideoListItem key={video.etag} video={video} />;
   });
 
   return (
-    <ul className="col-md-4 list-group">
+    <ul className="col-lg-4 list-group">
       {videoItems}
     </ul>
   );
