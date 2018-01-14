cab6edbc7aceaab807613a0074aa274802c6baf7
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-01-14 18:31:39 -0200

    Add a loading message in VideoDetail

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 const VideoDetail = ({video}) => {
+  if(!video) {
+    return <div>Loading...</div>;
+  }
+
   const videoId = video.id.videoId;
   const url = `https://www.youtube.com/embed/${videoId}`;
 
   return (
-    <div className="video-detail col-md-8">
-      <div className="ember-responsive embed-responsive-16by9">
+    <div className="video-detail col-lg-8">
+      <div className="embed-responsive embed-responsive-16by9">
         <iframe className="embed-responsive-item" src={url}></iframe>
       </div>
       <div className="details">
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCe7DUSCBFIblEmX0nSMS-xsWYQKDry3SQ';
 
@@ -22,6 +23,7 @@ class App extends Component {
     return (
        <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} /> {/* Render the details of the first video of the array */}
         <VideoList videos={this.state.videos} />
       </div>
     );
