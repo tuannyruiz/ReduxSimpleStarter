e976009c49a2abb9a067864865ad4bfad86d82e3
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-01-20 18:04:49 -0200

    Implement video selector

--- src/index.js
+++ src/index.js
@@ -11,10 +11,16 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {videos: []};
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
   
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({videos}); // this.setState({videos: videos})
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
 
   }
@@ -23,8 +29,10 @@ class App extends Component {
     return (
        <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]} /> {/* Render the details of the first video of the array */}
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />  
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
